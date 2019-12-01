0fa7d2334921af1c8404501d9c601066f1a975e5
giancarlov <giancarlovq@outlook.es>
2019-11-30 21:06:28 -0500

    Implementación de URL y VIEWS - APP PROFILES

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+class Register(TemplateView):
+    template_name = 'profiles/register.html'
