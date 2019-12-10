91c6f1855d15b1183527409430291a4c0d26cb03
giancarlov <giancarlovq@outlook.es>
2019-12-10 11:47:18 -0500

    The App-Blog model was modified

--- blog/models.py
+++ blog/models.py
@@ -3,31 +3,30 @@ from django.db import models
 
 
 # Create your models here.
-class Publicaction(models.Model):
+class Category(models.Model):
+    name = models.CharField(max_length = 300)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Publication(models.Model):
     title = models.CharField(max_length = 200, unique = True)
     subtitle = models.CharField(max_length = 200, blank = True)
     content = models.TextField()
-    publication_status = models.CharField(max_length = 50)
+    status = models.CharField(max_length = 50)
     information_sources = models.TextField()
-    publication_image = models.ImageField()
-    publication_date = models.DateTimeField(auto_now_add = now)
-    publication_updated = models.DateTimeField(auto_now = now)
+    image = models.ImageField(height_field = 640, width_field = 480)
+    date = models.DateTimeField(auto_now_add = now)
+    updated = models.DateTimeField(auto_now = now)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
 
     class Meta:
-        ordering = [ '-publication_date' ]
+        ordering = [ '-date' ]
+
         # plural publications
 
-class Category(models.Model):
-    publication_category = models.ForeignKey(Publicaction, on_delete = models.CASCADE)
 
-"""
-category
-# title
-# subtitle
-# content
-# image
-# publication_date
-# update_date
-# source
-# state
-"""
