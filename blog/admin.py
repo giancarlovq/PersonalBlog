f79303b668b6ab538b27886997f278adc7737ae0
giancarlov <giancarlovq@outlook.es>
2019-12-12 21:12:27 -0500

    Implementation of the URL and Views of the App-Blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Category, Publication
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+
+class PublicationAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    list_display = ('title', 'published', 'display_categories', 'author', 'status')
+    ordering = ('author', 'published')
+    search_fields = ('title', 'content', 'author__username', 'category__name')
+    date_hierarchy = 'published'
+    list_filter = ('author__username', 'category__name')
+
+    def display_categories(self, obj):
+        return ', '.join([category.name for category in obj.category.all().order_by('name')])
+
+
+    display_categories.short_description = 'Categories'
+
 
 # Register your models here.
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Publication, PublicationAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Publication
+
 
 # Create your views here.
+def blog(request):
+    publications = Publication.objects.all()
+    return render(request, 'blog/blog.html', {'publications': publications})
