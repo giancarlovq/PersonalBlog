0c8e57158346317cff7e201d1da3da65e6670ba6
giancarlov <giancarlovq@outlook.es>
2019-12-12 21:04:45 -0500

    Creation and implementation of the App-Blog Model(DB)

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,15 @@
-from django.utils.timezone import now
 from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length = 300)
+    name = models.CharField(max_length = 100)
+    created = models.DateTimeField(auto_now_add = True)
 
     class Meta:
+        verbose_name_plural = 'Categories'
         ordering = ['name']
 
     def __str__(self):
@@ -15,18 +18,20 @@ class Category(models.Model):
 
 class Publication(models.Model):
     title = models.CharField(max_length = 200, unique = True)
-    subtitle = models.CharField(max_length = 200, blank = True)
+    subtitle = models.CharField(max_length = 200)
     content = models.TextField()
-    status = models.CharField(max_length = 50)
-    information_sources = models.TextField()
-    image = models.ImageField(height_field = 640, width_field = 480)
-    date = models.DateTimeField(auto_now_add = now)
-    updated = models.DateTimeField(auto_now = now)
-    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    published = models.DateTimeField(default = now)
+    status = models.CharField(max_length = 50, null = True, blank = True)
+    sources = models.TextField(null = True, blank = True)
+    image = models.ImageField(upload_to = 'blog')
+    created = models.DateTimeField(default = now)
+    updated = models.DateTimeField(auto_now = True)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    category = models.ManyToManyField(Category, related_name = 'get_publications')
 
     class Meta:
-        ordering = [ '-date' ]
+        verbose_name_plural = 'Publications'
+        ordering = ['-created']
 
-        # plural publications
-
-
+    def __str__(self):
+        return self.title
